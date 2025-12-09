b205121dcdd96bc7ce6c711ec3fff040dce496af
Connor Francis <connorfrancis@live.com>
2025-12-08 20:14:52 -0700

    exercise completed

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1
+
+code1 = 7+3
+
+let code2 = 100 - 40 - (100/5)
+
+let code3 = (3%2)+38
+
+let message = "The vault has been secured. The combination is:"
+
+let codeA = code1+'-'+code2+'-'+code3
+
+let codeB = `${code1}-${code2}-${code3}`
+
+
+console.log(message, codeA, codeB)
